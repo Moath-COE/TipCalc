d282b3703bc9070bd9ad70f5d89ba5a2acd337d9
Moath Abdelaziz <93261842+Moath-COE@users.noreply.github.com>
2023-05-20 14:25:11 +0300

    most of the functionality

---
+++ JS/index.js
@@ -0,0 +1,69 @@
+const billAmount = document.getElementById("bill-in-box");
+const btns = document.querySelector(".tip-btns");
+const peopleNum = document.getElementById("pnum-in-box");
+const customFactor = document.getElementById("tip-custom");
+const valueTotal = document.getElementById("value-total");
+const valueAmount = document.getElementById("value-amount");
+const resetBtn = document.getElementById("reset");
+let amount = 0;
+let pNum = 0;
+let factor = 0;
+
+billAmount.addEventListener("change", (e) => {
+  e.preventDefault();
+  amount = e.target.value;
+  valid();
+});
+
+customFactor.addEventListener("change", (e) => {
+  e.preventDefault();
+  for (y of btns.children) {
+    y.firstElementChild.attributes.active.nodeValue = "false";
+  }
+  customFactor.firstElementChild.attributes.active.nodeValue = "true";
+  factor = e.target.value / 100;
+  valid();
+});
+
+btns.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    customFactor.firstElementChild.value = "";
+    for (y of btns.children) {
+      y.firstElementChild.attributes.active.nodeValue = "false";
+    }
+    e.target.attributes.active.nodeValue = "true";
+    factor = e.target.dataset.value;
+    valid();
+  }
+});
+
+peopleNum.addEventListener("change", (e) => {
+  e.preventDefault();
+  pNum = e.target.value;
+  valid();
+});
+
+function valid() {
+  if (amount != 0 && factor != 0 && pNum != 0) {
+    let x = (Number.parseInt(amount) + amount * factor).toFixed(2);
+    valueAmount.innerHTML = x;
+    valueTotal.innerHTML = (x / pNum).toFixed(2);
+    resetBtn.attributes.active.nodeValue = "true";
+  }
+}
+
+resetBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  valueAmount.innerHTML = "0.00";
+  valueTotal.innerHTML = "0.00";
+  for (y of btns.children) {
+    y.firstElementChild.attributes.active.nodeValue = "false";
+  }
+  customFactor.firstElementChild.value = "";
+  billAmount.value = "";
+  peopleNum.value = "";
+  amount = 0;
+  pNum = 0;
+  factor = 0;
+  resetBtn.attributes.active.nodeValue = "false";
+});
