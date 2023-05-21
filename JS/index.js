54fa89d9a3149cf2bc3ec1842ec4fb2981bd5e7e
Moath Abdelaziz <93261842+Moath-COE@users.noreply.github.com>
2023-05-21 13:56:18 +0300

    final draft 1

--- JS/index.js
+++ JS/index.js
@@ -6,6 +6,8 @@ const customFactor = document.getElementById("tip-custom");
 const valueTotal = document.getElementById("value-total");
 const valueAmount = document.getElementById("value-amount");
 const resetBtn = document.getElementById("reset");
+const error1 = document.querySelector(".e1");
+const error2 = document.querySelector(".e2");
 let amount = 0;
 let pNum = 0;
 let factor = 0;
@@ -15,24 +17,42 @@ let factor = 0;
 // amount input box handler
 billAmount.addEventListener("change", (e) => {
   e.preventDefault();
-  amount = e.target.value;
-  valid();
+  if (e.target.value > 0) {
+    error1.innerText = "";
+    e.target.classList.remove("error-border");
+    e.target.setCustomValidity("");
+    amount = e.target.value;
+    valid();
+  } else {
+    e.target.value = "";
+    e.target.setCustomValidity("please enter a valid amount");
+    e.target.classList.add("error-border");
+    error1.innerText = "Invalid input";
+  }
 });
 
 // custom tip amount handler
 customFactor.addEventListener("change", (e) => {
-  e.preventDefault();
-  for (y of btns.children) {
-    y.firstElementChild.attributes.active.nodeValue = "false";
+  if (e.target.value > 0 && e.target.value < 100) {
+    e.target.classList.remove("error-border");
+    e.target.setCustomValidity("");
+    for (y of btns.children) {
+      y.firstElementChild.attributes.active.nodeValue = "false";
+    }
+    customFactor.firstElementChild.attributes.active.nodeValue = "true";
+    factor = e.target.value / 100;
+    valid();
+  } else {
+    e.target.value = "";
+    e.target.setCustomValidity("please enter a number from 1 to 99");
+    e.target.classList.add("error-border");
   }
-  customFactor.firstElementChild.attributes.active.nodeValue = "true";
-  factor = e.target.value / 100;
-  valid();
 });
 
 // tip amount buttons handler
 btns.addEventListener("click", (e) => {
   if (e.target.nodeName === "BUTTON") {
+    customFactor.firstElementChild.classList.remove("error-border");
     customFactor.firstElementChild.value = "";
     for (y of btns.children) {
       y.firstElementChild.attributes.active.nodeValue = "false";
@@ -46,16 +66,25 @@ btns.addEventListener("click", (e) => {
 // number of people input box handler
 peopleNum.addEventListener("change", (e) => {
   e.preventDefault();
-  pNum = e.target.value;
-  valid();
+  if (!e.target.validity.stepMismatch) {
+    e.target.classList.remove("error-border");
+    e.target.setCustomValidity("");
+    error2.innerHTML = "";
+    pNum = e.target.value;
+    valid();
+  } else {
+    e.target.setCustomValidity("please enter a whole number");
+    e.target.classList.add("error-border");
+    error2.innerText = "Invalid input";
+  }
 });
 
 // updating results function
 function valid() {
   if (amount != 0 && factor != 0 && pNum != 0) {
     let x = (Number.parseInt(amount) + amount * factor).toFixed(2);
-    valueAmount.innerHTML = x;
-    valueTotal.innerHTML = (x / pNum).toFixed(2);
+    valueAmount.innerHTML = (x / pNum).toFixed(2);
+    valueTotal.innerHTML = x;
     resetBtn.attributes.active.nodeValue = "true";
   }
 }
